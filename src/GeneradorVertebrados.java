f87a9ef4d9f16b20f58c8dbff44fb660bdf46a86
César Cervantes Camargo <cncervantes@hotmail.com>
2021-09-17 20:39:47 -0500

    Commit 3

--- src/GeneradorVertebrados.java
+++ src/GeneradorVertebrados.java
@@ -4,6 +4,8 @@ public class GeneradorVertebrados {
         miVertebrados[0] = new Vertebrado("Camello", "Mamifero");
         miVertebrados[1] = new Vertebrado("Vaca", "Mamifero");
         miVertebrados[2] = new Vertebrado("Perro", "Mamifero");
+        miVertebrados[3] = new Vertebrado("Gato", "Mamifero");
+        miVertebrados[4] = new Vertebrado("Caballo", "Mamifero");
 
         return miVertebrados;
     }
